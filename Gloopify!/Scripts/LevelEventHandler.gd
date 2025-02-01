9743f30afe69eae7e7f82f7dfe1aa31055434b2c
Wizpiggy144 <kaymaku144@gmail.com>
2025-02-01 01:44:48 -0700

    Added reset button in options menu

--- Gloopify!/Scripts/LevelEventHandler.gd
+++ Gloopify!/Scripts/LevelEventHandler.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const LEVELS = 1
 var saved_data = {}
 
 func _ready() -> void:
@@ -7,7 +8,6 @@ func _ready() -> void:
 	var json = JSON.new()
 	var _result = json.parse(save_file.get_line())
 	saved_data = json.get_data()
-	print(saved_data)
 	
 func complete_level(level, time, c1, c2, c3, c4, c5):
 	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
@@ -32,3 +32,23 @@ func complete_level(level, time, c1, c2, c3, c4, c5):
 	var json_data = JSON.stringify(data)
 	save_file.store_line(json_data)
 	
+func reset_data():
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var data = {}
+	for i in range(1, LEVELS + 2):
+		var levelID = "level" + str(i)
+		data[levelID] = {
+			"clear": false,
+			"record": -1,
+			"c1": false,
+			"c2": false,
+			"c3": false, 
+			"c4": false,
+			"c5": false
+		}
+	data["PlayerStats"] = {
+		"coins": 0
+		#Add other lines here as upgrades unlock
+	}
+	var json_data = JSON.stringify(data)
+	save_file.store_line(json_data)
--- Gloopify!/UI/options.gd
+++ Gloopify!/UI/options.gd
@@ -5,3 +5,20 @@ func _on_volume_value_changed(value: float) -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/Main_Menu.tscn")
+
+
+func _on_reset_pressed() -> void:
+	var rect = get_node("ColorRect")
+	rect.show()
+	
+
+
+func _on_yes_pressed() -> void:
+	var handler = get_node("/root/LevelEventHandler")
+	handler.reset_data()
+	get_tree().change_scene_to_file("res://UI/Main_Menu.tscn")
+
+
+func _on_no_pressed() -> void:
+	var rect = get_node("ColorRect")
+	rect.hide()
