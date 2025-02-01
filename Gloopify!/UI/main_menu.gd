c7403f60edd5d9598f34da7525dff6af2bc734a0
Kyle Herman <117727568+VoidedCrystal@users.noreply.github.com>
2025-01-31 20:01:34 -0700

    Added Main Menu
    Play and Exit Button are functional. Options will be made function when we figure out what to add to it

---
+++ Gloopify!/UI/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/Level1.tscn")
