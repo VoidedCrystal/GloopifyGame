e874be216e6276066a921467098f3b919937ed0b
Wizpiggy144 <kaymaku144@gmail.com>
2025-01-31 18:47:00 -0700

    Added player acceleration

--- Gloopify!/Entities/player.gd
+++ Gloopify!/Entities/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const BASE_SPEED = 275.0
 const JUMP_VELOCITY = -400.0
+const MAX_SPEED = 1000.0
 
+var speed = 275.0
+var current_direction = 0
+var last_direction = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,11 +18,25 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move-left", "move-right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		current_direction = 0
+		speed = BASE_SPEED
+	
+	if Input.is_action_just_pressed("move-left"):
+		current_direction = -1
+	elif Input.is_action_just_pressed("move-right"):
+		current_direction = 1
+		
+	#increase speed if necessary
+	if current_direction == last_direction and speed < MAX_SPEED:
+		speed += 5
+	elif current_direction != last_direction:
+		speed = BASE_SPEED
 
 	move_and_slide()
+
+	last_direction = current_direction
