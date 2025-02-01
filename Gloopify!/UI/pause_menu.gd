465860a235a3d2c99549af0ea4044d84928a3759
Wizpiggy144 <kaymaku144@gmail.com>
2025-01-31 23:31:27 -0700

    Resize UI and clean up files

--- Gloopify!/UI/pause_menu.gd
+++ Gloopify!/UI/pause_menu.gd
@@ -1,14 +1,28 @@
 extends Control
 
-@onready var main = $"../Levels/test.tscn"
+var paused = false
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_resume_pressed() -> void:
-	main.pauseMenu()
+	pauseMenu()
 
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/Main_Menu.tscn")
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+	
+func pauseMenu():
+	if paused:
+		hide()
+		Engine.time_scale = 1
+	else:
+		show()
+		Engine.time_scale = 0
+	
+	paused = !paused
