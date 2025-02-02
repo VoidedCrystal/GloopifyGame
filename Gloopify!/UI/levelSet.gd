752f4c4d8e6d7f079453f46b11b1c2b4b12d5ecb
Wizpiggy144 <kaymaku144@gmail.com>
2025-02-01 23:35:47 -0700

    updated level select page

--- Gloopify!/UI/levelSet.gd
+++ Gloopify!/UI/levelSet.gd
@@ -3,6 +3,8 @@ extends Control
 class_name LevelSet
 
 @export var level_name:="1"
+@export var level_art: Texture2D
+@export var level_art_grey: Texture2D
 @export_file("*.tscn") var next_scene_path: String
 @export var next_level_right: LevelSet
 @export var next_level_left: LevelSet
@@ -10,7 +12,18 @@ class_name LevelSet
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Label.text = "Level " + str(level_name)
-
+	$Sprite2D.texture = level_art
+	$Sprite2D2.texture = level_art_grey
+	if level_name == "1":
+		$Label2.text = "Jessica is being chased by the police as she flees toward the nearest bakery. It just so happens that it sits atop a massive cliff no ordinary person could scale. Fortunately, Jessica is anything but ordinary."
+	elif level_name == "2":
+		$Label2.text = "A few days later, Jessica has thrown off the police. However, she is running low on power. Feeling desperate, she takes a shortcut through a sketchy area in hopes of finding a bakery."
+	elif level_name == "3":
+		$Label2.text = "Jessica is a natural daredevil, always showing off and taking risks to evade capture. It only makes sense, then, that she feels drawn to the junkyard, a place full of machinery, spikes, and most of all, danger. Of course, after a fun time in the junkyard, she obviously wants a cupcake to cool down."
+	elif level_name == "4":
+		$Label2.text = ""
+	elif level_name == "5":
+		$Label2.text = ""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
--- Gloopify!/UI/level_select.gd
+++ Gloopify!/UI/level_select.gd
@@ -5,16 +5,18 @@ class_name LevelSelect
 var current_world: int = 0
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_left") and current_level.next_level_left:
+	if event.is_action_pressed("move-left") and current_level.next_level_left:
 		current_level.hide()
 		current_level = current_level.next_level_left
 		current_level.show()
-	if event.is_action_pressed("ui_right") and current_level.next_level_right:
+	if event.is_action_pressed("move-right") and current_level.next_level_right:
 		current_level.hide()
 		current_level = current_level.next_level_right
 		current_level.show()
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("accept"):
 		if current_level.next_scene_path:
 			get_tree().change_scene_to_file(current_level.next_scene_path)
+	if Input.is_action_pressed("pause"):
+		get_tree().change_scene_to_file("res://UI/Main_Menu.tscn")
 			
 			
